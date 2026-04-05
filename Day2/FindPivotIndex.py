0bf19085a14f5127472a42ddab7bd1d609268f50
Joshika-95 <joshikasenthilkumar2007@gmail.com>
2026-04-05 21:23:52 +0530

    FindPivotIndex.py

--- Day2/FindPivotIndex.py
+++ Day2/FindPivotIndex.py
@@ -1,13 +1,11 @@
 def pivotIndex(nums):
+    total = sum(nums)
+    left_sum=0
     for i in range(len(nums)):
-        left_sum=0
-        right_sum=0
-        for j in range(i):
-            left_sum+=nums[j]
-            for i in range(i+1,len(nums)):
-                right_sum+=nums[i]
-            if left_sum==right_sum:
-                return i
-            return -1
+        right_sum=total-left_sum-nums[i]
+        if left_sum==right_sum:
+            return i
+        left_sum+=nums[i]
+    return -1
 nums=[1,7,3,6,5,6]
 print(pivotIndex(nums))
